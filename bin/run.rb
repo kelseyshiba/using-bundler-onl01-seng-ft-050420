bf1b958f524a579a000f344445441d407a3e0f75
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-05-14 21:44:00 +0000

    Automatically backed up by Learn

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,5 @@
-Hashie
-Sinatra
-Octokit
-Pry
-AwesomePrint
+require_relative 'Hashie'
+require_relative 'Sinatra'
+require_relative 'Octokit'
+require_relative 'Pry'
+require_relative 'AwesomePrint'
